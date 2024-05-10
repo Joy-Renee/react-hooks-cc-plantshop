3df2807ceebdf527dbc7e0ffa8feb8462901b966
Joy Renee Mugo <mugojoy24@gmail.com>
2024-05-10 20:52:15 +0300

    done

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { PlantContext } from "./App";
 
 function Search() {
+  const { search, handleSearchChange } = useContext(PlantContext);
   return (
     <div className="searchbar">
       <label htmlFor="search">Search Plants:</label>
       <input
+        value={search}
         type="text"
         id="search"
         placeholder="Type a name to search..."
-        onChange={(e) => console.log("Searching...")}
-      />
-    </div>
-  );
-}
+        onChange={handleSearchChange}
+        />
+      </div>
+    );
+  }
 
-export default Search;
+  export default Search;
